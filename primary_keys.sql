54f3d1082cd286dc9ceb8fec4005034121e4f70e
sparoodata <sparoodata@gmail.com>
2025-09-30 23:26:37 -0700

    Add files via upload

---
+++ primary_keys.sql
@@ -0,0 +1,16 @@
+WITH relevant_schemas AS (
+  SELECT n.oid AS namespace_oid, n.nspname
+  FROM pg_namespace n
+  WHERE nspname IN (:include_schemas)
+)
+SELECT
+  s.nspname::text || '.' ||
+  rel.relname::text || '.' ||
+  con.contype::text || '.' ||
+  con.conname::text AS primary_key
+FROM pg_constraint con
+JOIN relevant_schemas s ON s.namespace_oid = con.connamespace
+JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid
+WHERE con.contype = 'p'
+ORDER BY 1;
+
